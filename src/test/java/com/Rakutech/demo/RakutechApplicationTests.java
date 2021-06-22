17756678234c068e944b5dfa409e39ddc0b3dec3
Victor Chiong <cjvicro@gmail.com>
2021-06-22 21:01:16 +0800

    init

---
+++ src/test/java/com/Rakutech/demo/RakutechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Rakutech.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RakutechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
